1fb8e2a77c4b3f4078f8c658beb49e71e6f8eaa0
germanps <gpla@pasiona.com>
2018-03-15 09:11:12 +0100

    Strings methods

---
+++ src/js/02_strings.js
@@ -0,0 +1,15 @@
+let aprendiendo = 'Aprendiendo',
+    tecnologia = 'Javascript ',
+    mensaje = 'Aprendiendo JavasScript, CSS y HTML';
+
+console.log(tecnologia.length);
+console.log(tecnologia.concat(' ', 'es la ostia!'));
+console.log(tecnologia.toUpperCase());
+console.log(tecnologia.toLocaleLowerCase());
+console.log(mensaje.indexOf('JavasScript'));
+console.log(mensaje.substring(0,10));
+console.log(mensaje.slice(2));
+console.log(mensaje.split(' '));
+console.log(mensaje.replace('css','php'));
+console.log(mensaje.includes('CSS'));
+console.log(tecnologia.repeat(10));
