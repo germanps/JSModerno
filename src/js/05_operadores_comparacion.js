1001981e5a30d5c1acd58b0cc3cc164f5703f4c4
germanps <gpla@pasiona.com>
2018-03-16 13:43:03 +0100

    comparison operators

---
+++ src/js/05_operadores_comparacion.js
@@ -0,0 +1,11 @@
+const numero1 = 20;
+const numero2 = 50;
+const numero3 = '20';
+
+console.log( numero1 < numero2);
+console.log( numero1 == numero3);
+console.log( numero1 === numero2);
+console.log( numero1 != numero2);
+console.log('hola' == ' hola')
+console.log('a' > 'b');
+console.log( null == undefined);
