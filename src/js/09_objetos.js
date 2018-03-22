b8182987c6bf411066b4f9263336b3252019f2fc
germanps <gpla@pasiona.com>
2018-03-22 08:06:59 +0100

    Objetos 2

--- src/js/09_objetos.js
+++ src/js/09_objetos.js
@@ -20,3 +20,15 @@ console.log(persona);
 console.log(persona.musica);
 console.log(persona.hogar.ciudad);
 console.log(persona.nacimiento());
+
+//Array de Objetos
+const coches =  [
+   {modelo: 'Mustang', motor: 6.1},
+   {modelo: 'Camaro', motor: 6.5},
+   {modelo: 'Challenger', motor: 6.3}
+]
+coches[0].modelo = 'Audi';
+
+for (let i = 0; i < coches.length; i++) {
+   console.log(`Modelo: ${coches[i].modelo} # Motor: ${coches[i].motor}`);
+}
