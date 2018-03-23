40c14d890e630c981f2f198fe7884ee0ac09b15f
germanps <gpla@pasiona.com>
2018-03-23 07:49:48 +0100

    Funciones 2

--- src/js/10_funciones.js
+++ src/js/10_funciones.js
@@ -20,3 +20,27 @@ const suma = function(a = 3, b = 10) {
 }
 console.log( suma(1,2) );
 console.log( suma(5) );
+
+//IIFE (autoejecutables)
+(function(tecnologia){
+   console.log('Creando función autoejecutable (IIFE)')
+   console.log(`Aprendiendo ${tecnologia} moderno`);
+})('Javascript');
+
+//Métodos de propiedad 
+const musica = {
+   reproducir: function(id){
+      console.log(`Reproduciendo Música con ID ${id}`);
+   },
+   pausar: function(){
+      console.log(`Pausa la Música`);
+   }
+}
+musica.reproducir(30);
+musica.pausar();
+//Los métodos de objetos tb pueden crearse fuera del objeto
+musica.borrar = function(id) {
+   console.log(`Borrando canción con el ID: ${id}`)
+}
+
+musica.borrar(20);
