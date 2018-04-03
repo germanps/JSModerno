5d9de665f5442a24ce0dfab40c76fa769f5acb3e
germanps <gpla@pasiona.com>
2018-04-03 08:39:24 +0200

    Local storage && new project

---
+++ src/pages/local_storage/assets/js/app.js
@@ -0,0 +1,30 @@
+//Variables
+const listaTweets = document.querySelector('#lista-tweets');
+
+
+
+
+//Event listenerts
+eventListeners();
+function eventListeners(){
+   //Cuando se envía el formulario
+   document.querySelector('#formulario').addEventListener('submit',agregarTweet,false);
+}
+
+//Functions
+function agregarTweet (e) {
+   e.preventDefault();
+   //leer valor del textarea
+   const tweet = document.getElementById('tweet').value;
+
+   //crear elemento borrar
+   const botonBorrar = document.createElement('a');
+   botonBorrar.classList = 'borrar-tweet';
+   botonBorrar.innerText = 'X';
+
+   //crear elemento y añadirlo a la lista
+   const li = document.createElement('li');
+   li.innerText = tweet;
+   li.appendChild(botonBorrar);
+   listaTweets.appendChild(li);
+}
