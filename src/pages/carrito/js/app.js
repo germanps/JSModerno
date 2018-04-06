51cf3f260213edea57d4ead275d3e5bd6a585986
germanps <gpla@pasiona.com>
2018-04-06 09:12:30 +0200

    Insertar curso al carrito(DOM)

--- src/pages/carrito/js/app.js
+++ src/pages/carrito/js/app.js
@@ -1,8 +1,7 @@
 //Variables
 const carrito = document.getElementById('carrito');
 const cursos = document.getElementById('lista-cursos');
-
-
+const listaCursos = document.querySelector('#lista-carrito tbody');
 
 //Listeners
 cargaEventListeners();
@@ -12,18 +11,42 @@ function cargaEventListeners(){
 }
 
 
-
-
 //Funciones
 //Añade el curso al carrito
 function comprarCurso(e){
    e.preventDefault();
+   // Delegation para agregar-carrito
    if(e.target.classList.contains('agregar-carrito')){
       const curso = e.target.parentElement.parentElement;
+      //Enviamos el curso seleccionado para obtener sus datos
       leerDatosCurso(curso);
    }
 }
 //Lee los datos del curso
 function leerDatosCurso (curso) {
+   //Crear objeto del curso
+   const infoCurso = {
+      imagen: curso.querySelector('img').src,
+      titulo: curso.querySelector('h4').textContent,
+      precio: curso.querySelector('.precio span').textContent,
+      id: curso.querySelector('a').getAttribute('data-id')
+   }
+   insetarCarrito(infoCurso);
+}
+
+function insetarCarrito (curso) {
+   const row = document.createElement('tr');
    console.log(curso);
+   row.innerHTML = `
+      <td>
+         <img src='${curso.imagen}'>
+      </td>
+      <td>${curso.titulo}</td>
+      <td>${curso.precio}</td>
+      <td>
+         <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
+      </td>
+   `;
+   listaCursos.appendChild(row);
+   console.log(curso.imagen);
 }
