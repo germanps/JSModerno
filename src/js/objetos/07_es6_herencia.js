1c664ae1740bb94777436cd5b442e04e264ce4d4
unknown <germanps@outlook.es>
2018-04-14 09:22:40 +0200

    ES6 Herencia

--- src/js/objetos/07_es6_herencia.js
+++ src/js/objetos/07_es6_herencia.js
@@ -1,43 +1,38 @@
 class Cliente {
-   constructor(nombre,apellido,saldo){
+   constructor(nombre,saldo){
       this.nombre = nombre;
-      this.apellido = apellido;
       this.saldo = saldo;
    }
 
    imprimirSaldo(){
       return `Tu saldo es de: ${this.saldo}`
    }
-
-   tipoCliente(){
-      let tipo;
-      if (this.saldo > 10000) {
-         tipo = 'Gold';
-      }else if (this.saldo > 5000) {
-         tipo = 'Platinum';
-      }else{
-         tipo = 'Normal'
-      }
-      return tipo;
-   }
-   returarSaldo(reintegro){
-      this.saldo -= reintegro;
-   }
    
    //Método Static(no hace falta instanciar para llamarlo)
    static bienvenida(){
-      return `Bienvenido al cajero`;
+      return `Bienvenido al cajero de Clientes`;
    }
 }
 const maria = new Cliente('Maria','Perez',10000);
 
-console.log(maria);
-maria.returarSaldo(300);
+class Empresa extends Cliente {
+    constructor(nombre, saldo, telefono, tipo){
+        //Va hacia el constructor del que hereda
+        super(nombre,saldo);
+        this.telefono = telefono;
+        this.tipo = tipo;
+    }
+     //Método Static(no hace falta instanciar para llamarlo)
+   static bienvenida(){
+    return `Bienvenido al cajero para Empresas`;
+ }
+}
+const empresa1 = new Empresa('Empresa1', 10000, 938082058, 'Desarrollo web');
+console.log(Empresa.bienvenida());
+console.log(empresa1);
+console.log(empresa1.imprimirSaldo());
 
+const cliente1 = new Cliente('Germán', 5000);
 console.log(Cliente.bienvenida());
-console.log(maria.imprimirSaldo());
-console.log(maria.tipoCliente());
-
-
-// Herencia
-
+console.log(cliente1);
+console.log(cliente1.imprimirSaldo());
