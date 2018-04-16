dc5f959896dea4d513aceff54d71aeee8562f107
Germán Pla <germanps@outlook.es>
2018-04-16 08:54:38 +0200

    Leer datos del formulario

--- src/pages/objetos/cotizaSeguro/js/app.js
+++ src/pages/objetos/cotizaSeguro/js/app.js
@@ -1,6 +1,54 @@
+/***** Clases ****/
+// Cotizador 
+function Seguro (marca, anio, tipo) {
+  this.marca = marca;
+  this.anio = anio;
+  this.tipo = tipo;
+}
+// Todo lo que se muestra
+function Interfaz(){} //Inicia vacio
+
+
+/***** Eventlisteners ****/
+const formulario = document.getElementById('cotizar-seguro');
+formulario.addEventListener('submit', enviaFormulario, false);
+window.addEventListener('load', rellenaSelectAnios, false);
+
+
+
+
+
+/**** Funciones ****/
+// Envia el formulario
+function enviaFormulario (e) {
+  e.preventDefault();
+  // Leer marca seleccionada del select
+  const marca = document.getElementById('marca');
+  const marcaSeleccionada = marca.options[marca.selectedIndex].value;
+  // Leer el año seleccionado del select
+  const anio = document.getElementById('anio');
+  const anioSeleccionado = anio.options[anio.selectedIndex].value;
+
+  // Lee el valor del radioButton
+  const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+
+  // Crear instancia de Interfaz
+  const interfaz = new Interfaz();
+  // Revisar que los campos no estén vacíos
+  if (marcaSeleccionada === '' || anioSeleccionado === '' || tipo === '') {
+    // Interfaz: Imprime un error
+    console.log('faltan datos');
+  }else{
+    // Interfaz: Imprime una vista correcta 
+    console.log('datos enviados!');
+  }
+
+}
+
+
 
 //Rellenamos el select con los años de los vehículos
-window.addEventListener('load', rellenaSelectAnios, false);
 function rellenaSelectAnios(){
   const max = new Date().getFullYear();//año máximo (año actual)
   const min = max - 20 //hace 20 años
