08d04503c81e22dc735d89bedad903b289cce444
Germán Pla <germanps@outlook.es>
2018-04-25 17:19:41 +0200

    Cargando JSON con Fetch api

--- src/pages/fetch_api/js/app.js
+++ src/pages/fetch_api/js/app.js
@@ -1,8 +1,12 @@
 
 document.getElementById('txtBtn').addEventListener('click', cargarTxt, false);
+document.getElementById('jsonBtn').addEventListener('click', cargarJSON, false);
 
+
+//Archivo TXT
 function cargarTxt(){
   fetch('datos.txt')
+  //El primer then redefinimos la conexion y como queremos traer los datos
     .then(function(response){
       return response.text();//como quieres los datos
     })
@@ -11,6 +15,29 @@ function cargarTxt(){
       document.getElementById('resultado').innerHTML = data;
     })
     .catch(function(error){
-      
+      console.log(error);
+    })
+}
+
+
+//Archivo JSON
+function cargarJSON(){
+  fetch('empleados.json')
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      let html = '<ul>';
+      data.forEach(function(el) {
+        html += `<li>
+                  <span>${el.nombre}: </span>
+                  <span>${el.puesto}</span>
+                </li>`;
+      });
+      html += '</ul>';
+      document.getElementById('resultado').innerHTML = html;
+    })
+    .catch(function(error){
+      console.log(error);
     })
 }
