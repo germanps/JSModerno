0b276d6b93eede4c50584e3f05e4189a905cdd6e
Germán Pla <germanps@outlook.es>
2018-04-25 11:24:30 +0200

    Promises

--- src/js/promises.js
+++ src/js/promises.js
@@ -1,1 +1,31 @@
-console.log('promises');
+// Promises
+
+//resolve
+const esperando = new Promise(function(resolve, reject){
+  //resolve es cuando se cumple el Promise
+  //reject es cuando no se cumple el Promise
+  setTimeout(function() {
+    resolve('Se ejecutó');
+  }, 5000);
+});
+
+esperando.then(function(mensaje){
+  console.log(mensaje);
+});//se ejecuta el resolve()
+
+
+//reject (simular un error)
+const aplicarDescuento = new Promise(function(resolve, reject){
+  const descuento = false;
+  if(descuento){
+    resolve('Descuento aplicado');
+  }else{
+    reject('Error aplicando el descuento');
+  }
+});
+
+aplicarDescuento.then(function(resultado){
+  console.log(resultado);
+}).catch(function(error){
+  console.log(error);
+});
