e33ecce020a04f354ec7780a55a91ff358f1df79
germanps <gpla@pasiona.com>
2018-05-10 07:56:03 +0200

    Leer datos formulario

--- src/pages/eventos/js/app.js
+++ src/pages/eventos/js/app.js
@@ -1,2 +1,18 @@
 const eventbrite = new EventBrite();
 const ui = new Interfaz();
+
+//Listener al buscador
+document.getElementById('buscarBtn').addEventListener('click', (e) => {
+   e.preventDefault();
+   const textoBuscador = document.getElementById('evento').value;
+   const categorias = document.getElementById('listado-categorias');
+   const catSeleccionada = categorias.options[categorias.selectedIndex].value;
+   
+
+   //control buscador
+   if(textoBuscador !== ''){
+      console.log(`${textoBuscador} => ${catSeleccionada}`);
+   }else{
+      console.log('error');
+   }
+}, false);
