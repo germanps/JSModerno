1f9a6a876115aad95c62fcc51281438d985054fc
germanps <gpla@pasiona.com>
2018-05-11 07:59:07 +0200

    Construir template e imprimir Mostrar eventos

--- src/pages/eventos/js/app.js
+++ src/pages/eventos/js/app.js
@@ -13,8 +13,13 @@ document.getElementById('buscarBtn').addEventListener('click', (e) => {
    if(textoBuscador !== ''){
       //hacer la mandanga
       eventbrite.obtenerEventos(textoBuscador, catSeleccionada)
-         .then(eventos => {
-            console.log(eventos);
+         .then(data => {
+            if (data.eventos.events.length > 0) {
+                  //Retorna resultados
+                  ui.mostrarEvento(data.eventos);
+            }else{
+                  ui.mostrarMensaje('No hay resultados', 'alert alert-danger mt-4');
+            }
          });
    }else{
       //mostrar mensaje error
--- src/pages/eventos/js/interfaz.js
+++ src/pages/eventos/js/interfaz.js
@@ -42,4 +42,34 @@ class Interfaz {
             alert.remove();
         }
     }
+    mostrarEvento(data){
+        const listaEventos = data.events;
+        //recorrer el array eventos y crear su templateç
+        console.log(listaEventos)
+        this.limpiarResultadoEventos();
+        listaEventos.forEach(evento => {
+            this.listado.innerHTML +=   `
+                <div class="col-md-4 mb-4>
+                    <div class="card">
+                        <div class="card-body">
+                            <img class="img-fluid mb-2" src="${evento.logo != null ? evento.logo.url : ''}">
+                        </div>
+                        <div class="card-body">
+                            <div class="card-text">
+                                <h2 class="text-center">${evento.name.text}</h2>
+                                <p calss="lead text-info">Información del evento</p>
+                                <p>${evento.description.text.substring(0, 280)}...</p>
+                                <span class="badge badge-primary">Capacidad del evento: ${evento.capacity}</span>
+                                <span class="badge badge-secondary">Fecha del evento: ${evento.start.local}</span>
+                                <a href="${evento.url}" target="_blank" class="btn btn-primary btn-block mt-4">Comprar entradas</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `
+        });
+    }
+    limpiarResultadoEventos(){
+        this.listado.innerHTML = '';
+    }
 }
