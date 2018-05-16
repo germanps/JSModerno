b712be71e6efe50cab00de08cfad97fc7e55b570
Germán Pla <germanps@outlook.es>
2018-05-16 16:17:54 +0200

    Imprimir pines en el mapa

--- src/pages/maps/js/ui.js
+++ src/pages/maps/js/ui.js
@@ -4,6 +4,7 @@ class Interfaz{
         this.api = new Api();
 
         //Inicializar y obrtener la propiedad del mapa
+        //this.latLng = {lat: 41.68, lng: 2};
         this.latLng = {lat: 41.68, lng: 2};
         this.mapa = new google.maps.Map(document.getElementById('mapa'), {
             center: this.latLng,
@@ -16,10 +17,39 @@ class Interfaz{
         this.api.obtenerDatos()
             .then(datos => {
                 const resultado = datos.respuestaJSON.elements
-                for (const dato of resultado) {
-                    console.log(dato);
-                }
+                this.mostrarMapa(resultado);
             });
     }
+    mostrarMapa(datos){
+        datos.forEach(dato => {
+            // Destructuring
+            let {
+                adreca_nom,
+                descripcio,
+                imatge,
+                horaris,
+                localitzacio,
+            } = dato;
+            let municipio = dato.rel_municipis.municipi_nom;
 
+            //crear objeto con latitud y longitud
+            let coordenadas = localitzacio.split(',');
+            let latLng = {
+                lat: Number(coordenadas[0]),
+                lng: Number(coordenadas[1])
+            }
+            //aseguramos que siempre venga un elemento con lat y lng
+            if(latLng.lat == ''){
+                return false;
+            }else{
+                console.log(latLng);
+                //Creamos el marker de google
+                let marker = new google.maps.Marker({
+                    position: latLng,
+                    map: this.mapa
+                });
+            }
+ 
+        });
+    }
 }
