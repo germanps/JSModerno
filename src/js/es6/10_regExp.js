c559310577b5eef3ec42609db67a99c47a85a8c3
Germán Pla <germanps@outlook.es>
2018-05-16 08:20:37 +0200

    ExpReg II

--- src/js/es6/10_regExp.js
+++ src/js/es6/10_regExp.js
@@ -24,3 +24,33 @@ console.log(expReg.test(valor));
 expReg = /\d+/;
 valor = '10:30 AM';
 console.log(expReg.test(valor));
+
+//Negar la expresion
+expReg = /[^0-9]/;
+valor = 1992;
+console.log(expReg.test(valor));
+
+//Sintaxis con llaves
+expReg = /\d{1,2}-\d{1,2}-\d{4}/;//rango de dígitos (desde hasta)
+valor = '10-10-2018';
+console.log(expReg.test(valor));
+
+//Evaluar letras o números
+expReg = /\w+/; //numeros o letras
+valor = 'Mensaje de prueba';
+console.log(expReg.test(valor));
+
+//Evaluar solo números
+expReg = /\d+/; //numeros
+valor = 1234;
+console.log(expReg.test(valor));
+
+//Evaluar Mayusculas
+expReg = /([A-Z])\w+/; //todas en letras mayusculas
+valor = 'jM';
+console.log(expReg.test(valor));
+
+//Evaluar Minusculas
+expReg = /([a-z])\w+/; //alguna en letras minusculas
+valor = 'MM';
+console.log(expReg.test(valor));
