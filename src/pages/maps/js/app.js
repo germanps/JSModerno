aae6ce6228f33194dcacf1914abb42c632d46ab3
germanps <gpla@pasiona.com>
2018-05-18 07:41:01 +0200

    Listener para el buscador

--- src/pages/maps/js/app.js
+++ src/pages/maps/js/app.js
@@ -1,3 +1,19 @@
 //Instanciar ambas clases
 const ui = new Interfaz();
-ui.mostrarSitiosInteres();
+
+//Mostrar sitios al cargar
+document.addEventListener('DOMContentLoaded', () => {
+   ui.mostrarSitiosInteres();
+})
+
+//Habilitar buscador
+const buscador = document.querySelector('#buscar input');
+//captar lo que escribimos en el input
+buscador.addEventListener('input', () => {
+   //si es mayor a 4 buscar sugerencias
+   if (buscador.value.length > 3) {
+      console.log('Buscando...');
+   }else{
+      console.log('Esperando...');
+   }
+})
